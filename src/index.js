0ee711ba946c8d15e726b189693aada089a2f89f
Michael Serna <michael.serna01@gmail.com>
2016-07-08 17:30:54 -0700

    Implemented functional and class based component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import SearchBar from "./components/search_bar";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyCK21wWAbN8uSxvP3EnK8DrSB4t6myf0eM';
+
+// Create a new compenent. Thid component should produce some html
+
+const App = () => {
+  return (
+    <div>
+    <SearchBar/>
+    </div>
+    );
+}
+
+//take this compenent's generated HTML and put it on the pafe (in the dom)
+
+ReactDOM.render(<App/>, document.querySelector(
+'.container'));
