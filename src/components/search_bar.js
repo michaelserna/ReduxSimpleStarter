241e0b6e1b96267010c51973f405807288d5ed54
Michael Serna <michael.serna01@gmail.com>
2016-07-11 15:09:05 -0700

    Finished working app with search feature

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,14 +8,17 @@ class SearchBar extends Component {
   }
 
   render(){
-    return (
-    <div>
-<input 
-value = {this.state.term}
-onChange = {event => this.setState({term: event.target.value })} />
-
-</div>
+  return (
+    <div className="search-bar">
+      <input 
+        value = {this.state.term}
+        onChange = {event => this.onInputChange(event.target.value)} />
+    </div>
     );
+  }
+  onInputChange(term){
+    this.setState({term})
+    this.props.onSearchTermChange(term);
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from "./components/search_bar";
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyCK21wWAbN8uSxvP3EnK8DrSB4t6myf0eM';
+const API_KEY = 'AIzaSyDDY2hW0KGcKYsNJGuERlKWoCdXw0Y_SNw';
+
+
+
 
 // Create a new compenent. Thid component should produce some html
+class App extends Component {
+  constructor(props){
+//props in a class based component are available anywhere as this.props
+    super(props);
 
-const App = () => {
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+       };
+    this.videoSearch('surfboards');
+    
+}
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      //update state with new list of videos
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });  
+  }
+  render(){
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 800);
+
   return (
     <div>
-    <SearchBar/>
+    <SearchBar onSearchTermChange = {videoSearch} />
+    <VideoDetail video = {this.state.selectedVideo}/>
+    <VideoList 
+    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+    videos = {this.state.videos} />
     </div>
-    );
+    );  
+  }
+
 }
 
 //take this compenent's generated HTML and put it on the pafe (in the dom)
